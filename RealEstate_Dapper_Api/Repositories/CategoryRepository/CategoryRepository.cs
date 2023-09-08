d3e0efcedfaf2e97944ad3ee91d70d916dec5763
osman28tr <osmantonbul2001@gmail.com>
2023-09-08 17:50:14 +0300

    Dapper konfigürasyon ayaları yapıldı ve kategori ile ilgili repositoryler eklendi.

---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly Context _context;
+
+        public CategoryRepository(Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<ResultCategoryDto>> GetAllAsync()
+        {
+            string query = "select * from Category";
+            using (var _connection = _context.CreateConnection())
+            {
+                var values = await _connection.QueryAsync<ResultCategoryDto>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<List<ResultCategoryDto>> GetAllAsync();
+    }
+}
