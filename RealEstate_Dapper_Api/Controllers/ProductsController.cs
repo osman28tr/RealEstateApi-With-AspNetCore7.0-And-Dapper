9c01e423a486570ce6e2956b6db8907684303249
osman28tr <osmantonbul2001@gmail.com>
2023-09-13 17:12:38 +0300

    Ürünlerin kategori bilgileri ile birlikte listelenmesi işlemi gerçekleştirildi.

---
+++ RealEstate_Dapper_Api/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.ProductRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductList()
+        {
+            var values = await _productRepository.GetAllAsync();
+            return Ok(values);
+        }
+        [HttpGet("api/products/ProductListWithCategory")]
+        public async Task<IActionResult> ProductListWithCategory()
+        {
+            var values = await _productRepository.GetAllProductWithCategory();
+            return Ok(values);
+        }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/ProductRepository/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.ProductDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.ProductRepository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly Context _context;
+
+        public ProductRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultProductDto>> GetAllAsync()
+        {
+            string query = "select * from product";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<List<ResultProductWithCategory>> GetAllProductWithCategory()
+        {
+            string query = "select ProductId,Title,Price,City,District,CategoryName from Product inner join Category on Product.CategoryId = Category.CategoryId";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductWithCategory>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
