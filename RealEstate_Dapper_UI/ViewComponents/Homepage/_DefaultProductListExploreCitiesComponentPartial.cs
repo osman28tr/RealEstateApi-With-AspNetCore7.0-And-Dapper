118b448ace6ee2950e458304ea83494627a9d37e
osman28tr <osmantonbul2001@gmail.com>
2023-09-13 18:25:32 +0300

    Anasayfanın yönetilebilirliği için componentlere ayrılması işlemi tamamlandı.

---
+++ RealEstate_Dapper_UI/ViewComponents/Homepage/_DefaultProductListExploreCitiesComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.Homepage
+{
+    public class _DefaultProductListExploreCitiesComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
