5c5435868424a714664e12aaee18a3ebc1454798
osman28tr <osmantonbul2001@gmail.com>
2023-09-18 01:23:22 +0300

    Api'nin consume edilmesi işlemi gerçekleştirildi.

--- RealEstate_Dapper_UI/ViewComponents/Homepage/_DefaultHomepageProductList.cs
+++ RealEstate_Dapper_UI/ViewComponents/Homepage/_DefaultHomepageProductList.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ProductDtos;
 
 namespace RealEstate_Dapper_UI.ViewComponents.Homepage
 {
     public class _DefaultHomepageProductList:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultHomepageProductList(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:60224/api/Products/api/products/ProductListWithCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDtos>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
