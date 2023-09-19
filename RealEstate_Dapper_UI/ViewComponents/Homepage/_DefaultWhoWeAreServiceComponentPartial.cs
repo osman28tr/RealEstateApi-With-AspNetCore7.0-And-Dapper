a78d0f96cd2fd385806805449acb67903e8e4f46
osman28tr <osmantonbul2001@gmail.com>
2023-09-19 17:09:15 +0300

    Whoweare service api consume edilmesi işlemi revize edildi.

---
+++ RealEstate_Dapper_UI/ViewComponents/Homepage/_DefaultWhoWeAreServiceComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.WhoWeAreDtos;
+
+namespace RealEstate_Dapper_UI.ViewComponents.Homepage
+{
+    public class _DefaultWhoWeAreServiceComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultWhoWeAreServiceComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:60224/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(value);
+            }
+            return View();
+        }
+    }
+}
