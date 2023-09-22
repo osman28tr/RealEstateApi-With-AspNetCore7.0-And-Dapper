7765fc9e9b0e3dc66ec844e8356054e15506e052
osman28tr <osmantonbul2001@gmail.com>
2023-09-22 15:23:39 +0300

    Populer lokasyonlar api ve ui işlemleri gerçekleştirildi.

---
+++ RealEstate_Dapper_Api/Repositories/PopularLocationRepositories/PopularLocationRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.BottomGridDtos;
+using RealEstate_Dapper_Api.Dtos.PopularLocationDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.PopularLocationRepositories
+{
+	public class PopularLocationRepository : IPopularLocationRepository
+	{
+		private readonly Context _context;
+
+		public PopularLocationRepository(Context context)
+		{
+			_context = context;
+		}
+
+		public async Task<List<ResultPopularLocationDto>> GetAllAsync()
+		{
+			string query = "select * from PopularLocations";
+			using (var _connection = _context.CreateConnection())
+			{
+				var values = await _connection.QueryAsync<ResultPopularLocationDto>(query);
+				return values.ToList();
+			}
+		}
+	}
+}
--- RealEstate_Dapper_UI/ViewComponents/Homepage/_DefaultProductListExploreCitiesComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/Homepage/_DefaultProductListExploreCitiesComponentPartial.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.PopularLocations;
 
 namespace RealEstate_Dapper_UI.ViewComponents.Homepage
 {
     public class _DefaultProductListExploreCitiesComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
-        {
-            return View();
-        }
-    }
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _DefaultProductListExploreCitiesComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("http://localhost:60224/api/PopularLocations/");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultPopularLocationDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
 }
